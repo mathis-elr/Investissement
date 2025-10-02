ac70d1c4bbe8628bfa25c4411b7ef748f250303f
mathis-elr <mathis.eloire15@gmail.com>
2025-10-02 14:43:29 +0200

    premier commit

---
+++ Investissement/Investissement/AjoutActif.cs
@@ -0,0 +1,113 @@
+﻿using ClosedXML.Excel;
+using MetroFramework.Controls;
+using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SQLite;
+
+
+namespace Investissement
+{
+    public partial class AjoutActif : MetroForm
+    {
+        public Form1 form;
+        public AjoutActif(Form1 form)
+        {
+            InitializeComponent();
+            this.form = form;
+        }
+
+        private void AjoutActif_Load(object sender, EventArgs e)
+        {
+            //fenetre
+            this.Text = "Ajouter un actif";
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            this.Size = new Size(250, 220);
+
+            //layout
+            FlowLayoutPanel layoutPrincipal = new FlowLayoutPanel();
+            layoutPrincipal.FlowDirection = FlowDirection.TopDown;
+            layoutPrincipal.Dock = DockStyle.Fill;
+            Padding padding = new Padding(10);
+
+            //input
+            MetroTextBox inputNom = new MetroTextBox();
+            inputNom.WaterMark = "Nom";
+            inputNom.Width = 100;
+            layoutPrincipal.Controls.Add(inputNom);
+
+            MetroTextBox inputType = new MetroTextBox();
+            inputType.WaterMark = "Type";
+            inputType.Width = 100;
+            layoutPrincipal.Controls.Add(inputType);
+
+            MetroTextBox inputISIN = new MetroTextBox();
+            inputISIN.WaterMark = "ISIN";
+            inputISIN.Width = 100;
+            layoutPrincipal.Controls.Add(inputISIN);
+
+            MetroTextBox inputRisque = new MetroTextBox();
+            inputRisque.WaterMark = "Risque (securitaire ou risque)";
+            inputRisque.Width = 100;
+            layoutPrincipal.Controls.Add(inputRisque);
+
+            //boutons
+            MetroButton btnAjout = new MetroButton();
+            btnAjout.Text = "Ajouter l'actif";
+            btnAjout.AutoSize = true;
+            btnAjout.UseCustomBackColor = true;
+            btnAjout.BackColor = System.Drawing.Color.LightGreen;
+            btnAjout.Click += (s, _) => AjouterActif(inputNom.Text, inputType.Text, inputISIN.Text, inputRisque.Text);
+            layoutPrincipal.Controls.Add(btnAjout);
+
+            //afficher les layouts
+            this.Controls.Add(layoutPrincipal);
+        }
+
+        private void AjouterActif(string nom, string type, string isin, string risque)
+        {
+
+            if (nom != "" && type != "")
+            {
+
+                /*-------------------------------
+                   Ajout dans la base de données
+                *-------------------------------*/
+
+                try
+                {
+                    string query = "INSERT INTO Actif VALUES(@nom,@type,@ISIN,@risque);";
+                    var command = new SQLiteCommand(query, form.bdd);
+                    command.Parameters.AddWithValue("@nom", nom);
+                    command.Parameters.AddWithValue("@type", type);
+                    command.Parameters.AddWithValue("@ISIN", isin);
+                    command.Parameters.AddWithValue("@risque", risque);
+
+                    command.ExecuteNonQuery();
+                    command.Dispose();
+                }
+                catch (SQLiteException ex)
+                {
+                    MessageBox.Show(ex.Message, "erreur insertion actif bdd");
+                }
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Erreur ajout actif : nom ou type vide / actif déjà existant");
+            }
+
+            //on met a jour le tableau en consequence de l'ajout
+            this.form.MajGridActifs();
+        }
+    }
+}
---
+++ Investissement/Investissement/Form1.cs
@@ -0,0 +1,291 @@
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Bibliography;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
+using MetroFramework.Controls;
+using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Investissement
+{
+    public partial class Form1 : MetroForm
+    {
+        /*ATTRIBUTS*/
+        public MetroGrid gridActifs;
+        public MetroDateTime date;
+        public SQLiteConnection bdd;
+        public MetroButton btnDernierInvest;
+
+        /*CONSTRUCTEUR*/
+        public Form1()
+        {
+            InitializeComponent();  
+            OuvrirBdd();
+            this.FormClosing += MyForm_FormClosing;
+        }
+
+        /*INITIALISATION*/
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //fenetre
+            this.Text = "Ajouter un investissement";
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+
+
+            //layout
+            TableLayoutPanel layoutPrincipal = new TableLayoutPanel()
+            {
+                Dock = DockStyle.Fill,
+                RowCount = 2,
+                ColumnCount = 2,
+                AutoSize = true
+            };
+
+            FlowLayoutPanel layout = new FlowLayoutPanel();
+            layout.FlowDirection = FlowDirection.LeftToRight;
+            layout.AutoSize = true;
+            layout.Padding = new Padding(10);
+
+            FlowLayoutPanel layoutActifs = new FlowLayoutPanel();
+            layoutActifs.FlowDirection = FlowDirection.LeftToRight;
+            layoutActifs.AutoSize = true;
+            layoutActifs.Padding = new Padding(10);
+
+            FlowLayoutPanel layoutBoutons = new FlowLayoutPanel();
+            layoutActifs.FlowDirection = FlowDirection.TopDown;
+            layoutActifs.AutoSize = true;
+            layoutActifs.Padding = new Padding(10);
+
+
+            //grille des actifs
+            this.gridActifs = new MetroGrid();
+            this.gridActifs.Columns.Add("Nom", "Nom actif");
+            this.gridActifs.Columns.Add("Type", "Type");
+            this.gridActifs.Columns.Add("quantite", "quantite (€)"); 
+            this.gridActifs.Columns.Add("prix", "prix (€)");
+            this.gridActifs.Width = 310;
+            this.gridActifs.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            this.gridActifs.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            MajGridActifs();
+
+
+            //entry date
+            this.date = new MetroDateTime();
+            this.date.Format = DateTimePickerFormat.Custom;
+            this.date.CustomFormat = "dd-MM-yyyy";
+            this.date.Width = 150;
+
+
+
+            //boutons
+            this.btnDernierInvest = new MetroButton();
+            this.btnDernierInvest.Text = "Afficher dernier investissement";
+            this.btnDernierInvest.AutoSize = true;
+            this.btnDernierInvest.UseCustomBackColor = true; //obligé sinon on peut pas modifier la couleur
+            this.btnDernierInvest.BackColor = System.Drawing.Color.LightBlue;
+            this.btnDernierInvest.Click += (s, _) => BtnDernierInvest();
+
+            MetroButton btnAjoutActif = new MetroButton();
+            btnAjoutActif.Text = "+";
+            btnAjoutActif.AutoSize = true;
+            btnAjoutActif.UseCustomBackColor = true;
+            btnAjoutActif.BackColor = System.Drawing.Color.Green;
+            btnAjoutActif.Click += BtnAjoutActif;
+
+            MetroButton btnInvest = new MetroButton();
+            btnInvest.Text = "Valider l'investissement";
+            btnInvest.AutoSize = true;
+            btnInvest.UseCustomBackColor = true; //obligé sinon on peut pas modifier la couleur
+            btnInvest.BackColor = System.Drawing.Color.LightGreen;
+            btnInvest.Click += (s, _) => BtnValiderInvest();
+
+
+            //mettre les elmts dans les layouts
+
+            layoutBoutons.Controls.Add(btnAjoutActif);
+            layoutBoutons.Controls.Add(btnDernierInvest);
+
+            layoutActifs.Controls.Add(this.gridActifs);
+
+            layout.Controls.Add(date);
+            layout.Controls.Add(btnInvest);
+
+            //mettre les layouts dans le layout principal
+            layoutPrincipal.Controls.Add(layoutActifs, 0, 0);
+            layoutPrincipal.Controls.Add(layoutBoutons, 1, 0);
+            layoutPrincipal.Controls.Add(layout, 0, 1);
+
+
+
+            //afficher les elmts
+            this.Controls.Add(layoutPrincipal);
+
+        }
+
+
+
+        /**************
+         ***METHODES***
+         **************/
+        public void OuvrirBdd()
+        {
+            try
+            {
+                string connectionString = @"Data Source=C:\Users\mathi\Documents\prog perso\c#\Investissement\bd\historique_transactions.db";
+                this.bdd = new SQLiteConnection(connectionString);
+
+                this.bdd.Open();
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.Message,"erreur ouverture bdd");
+            }
+        }
+
+
+        public void MajGridActifs()
+        {
+            this.gridActifs.Rows.Clear();
+
+            try
+            {
+                string query = "SELECT nom,type FROM Actif ORDER BY type;";
+                var command = new SQLiteCommand(query, bdd);
+
+                var actif = command.ExecuteReader(); //commande d'execution pour SELECT
+                
+
+                while (actif.Read())
+                {
+                    this.gridActifs.Rows.Add(actif.GetString(0), actif.GetString(1));
+                    //pour chaque actif ajouter son type dans une colonne a coté
+                }
+
+                actif.Dispose();
+
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.Message,"erreur selection bdd");
+            }
+        }
+
+
+        private void BtnValiderInvest()
+        {
+            foreach (DataGridViewRow transaction in this.gridActifs.Rows)
+            {
+                var date = this.date.Value;    
+                var actif = transaction.Cells[0].Value;
+                var type = transaction.Cells[1].Value;
+                var quantite = transaction.Cells[2].Value;
+                var prix = transaction.Cells[3].Value;
+                if (quantite != null)
+                {
+                    try
+                    {
+                        string query = "INSERT INTO [Transaction] (date,actif,type,quantite,prix) VALUES (@date,@actif,@type,@quantite,@prix);"; //entre crochets car Transaction est un mot réservé en sql
+                        var command = new SQLiteCommand(query, bdd);
+
+                        command.Parameters.AddWithValue("@date", date.ToString("yyyy-MM-dd"));
+                        command.Parameters.AddWithValue("@actif", actif.ToString());
+                        command.Parameters.AddWithValue("@type", type.ToString());
+                        command.Parameters.AddWithValue("@quantite", Convert.ToDouble(quantite));
+                        command.Parameters.AddWithValue("@prix", Convert.ToDouble(prix));
+
+                        command.ExecuteNonQuery(); //commande d'executuion poour INSERT, UPDATE, DELETE
+                        command.Dispose();
+                    }
+                    catch (SQLiteException ex)
+                    {
+                        MessageBox.Show(ex.Message,"erreur insertion transaction bdd");
+                    }
+                }
+
+            }
+        }
+
+        private void BtnAjoutActif(object sender, EventArgs e)
+        {
+            AjoutActif ajoutActif = new AjoutActif(this);
+            ajoutActif.Show();
+        }
+
+        private void BtnDernierInvest()
+        {
+            /*On récupère la date du dernier investissement*/
+            string queryDate = "SELECT date FROM [Transaction] ORDER BY date DESC LIMIT 1;"; 
+            var command = new SQLiteCommand(queryDate, bdd);
+            var date = command.ExecuteScalar(); //commande d'execution pour SELECT qui retourne qu'une seule valeur
+
+            //on varifie qu'il y a au moins une transaction
+            if (date != null) 
+            {
+                /*grace a cette date on recupère toutes les transactions effectuées ce jour là*/
+                string queryDernierInvest = "SELECT actif,type,quantite,prix FROM [Transaction] WHERE date=@date;";
+                var command2 = new SQLiteCommand(queryDernierInvest, bdd);
+                command2.Parameters.AddWithValue("@date", date);
+                var Investissements = command2.ExecuteReader();
+
+                //on parcours les transactions
+                while (Investissements.Read())
+                {
+                    this.gridActifs.Rows.Add(Investissements.GetString(0), Investissements.GetString(1), Investissements.GetDouble(2), Investissements.GetDouble(3));
+                }
+
+                command.Dispose();
+                command2.Dispose();
+
+                //on change le texte et l'action du bouton
+                this.btnDernierInvest.Text = "Afficher les actifs";
+                this.btnDernierInvest.Click += (s, _) => BtnInvestGeneral();
+            }
+            else
+            {
+                this.btnDernierInvest.BackColor = System.Drawing.Color.Red;
+            }
+        }
+
+        public void BtnInvestGeneral() 
+        { 
+            this.gridActifs.Rows.Clear(); //on vide le tableau
+
+            //on remet la grille des actifs
+            MajGridActifs();
+
+            //on change le texte et l'action du bouton
+            this.btnDernierInvest.Text = "Afficher dernier investissement";
+            this.btnDernierInvest.Click += (s, _) => BtnDernierInvest();
+        } 
+
+
+        private void MyForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (this.bdd != null && this.bdd.State == System.Data.ConnectionState.Open)
+                {
+                    this.bdd.Close();
+                    this.bdd.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erreur fermeture BDD");
+            }
+        }
+
+    }
+}
